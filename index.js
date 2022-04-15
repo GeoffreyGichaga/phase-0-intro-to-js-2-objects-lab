a92dda2870bb34ee1387f8cf544e844de85486b2
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-04-15 06:22:34 +0300

    commit tree f3adc44b301d0eb43ce3370d0da1b4b459ef4f71

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+const employee = {
+    name:"Jeff TJ",
+    streetAddress:"Ngong Road"
+}
+
+
+function updateEmployeeWithKeyAndValue(object,key,value)
+{
+    let updatedEmployee = {
+        ...object,
+        [key]:value
+
+    }
+    return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+{
+     employee[key] = value
+     return employee
+}
+
+function deleteFromEmployeeByKey(employee, key)
+{
+    let newEmployee={...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key)
+{
+    delete employee[key]
+    return employee
+     
+
+}
